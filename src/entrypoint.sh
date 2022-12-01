5b681e0265158fbf94a86d74775853c11481ed5a
Michael Yeaple <michael.yeaple@gmail.com>
2022-12-01 08:54:41 -0500

    Do not overwrite existing files when installing foundry.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -155,7 +155,7 @@ if [ $install_required = true ]; then
 
   if [ -f "${release_filename}" ]; then
     log "Installing Foundry Virtual Tabletop ${FOUNDRY_VERSION}"
-    unzip -q "${release_filename}" 'resources/*'
+    unzip -qn "${release_filename}" 'resources/*'
     log_debug "Installation completed."
   else
     log_error "Unable to install Foundry Virtual Tabletop!"
